83e675f40bc6a2829b2ceafc1b17f6210f0ac40a
gtarciso <gustavotarciso@hotmail.com>
2019-08-15 14:40:58 -0300

    desenhando ponto e reta

--- src/GUI.py
+++ src/GUI.py
@@ -13,13 +13,16 @@ class MainWindow:
 		self.builder = None
 		self.object_list = None
 		self.object_id = 0
-		self.object_list = None
+		self.darea = None
+		self.saved_objects = []
+
 
 	def run(self):
 		self.builder = Gtk.Builder()
 		self.builder.add_from_file("../ui/GUI.glade")
 		self.window = self.builder.get_object("main_window")
 		self.object_list = self.builder.get_object("liststore1")
+		self.darea = self.builder.get_object("viewport")
 
 
 		self.builder.connect_signals(MainWindowHandler(self))
@@ -33,6 +36,8 @@ class MainWindowHandler:
 		self.builder = main_window.builder
 		self.main_window = main_window
 		self.object_list = main_window.object_list
+		self.darea = main_window.darea
+		self.saved_objects = main_window.saved_objects
 
 		self.object_id = main_window.object_id
 
@@ -42,10 +47,37 @@ class MainWindowHandler:
 		self.builder.connect_signals(NewObjectHandler(self, self.object_window))
 
 		self.object_window.show_all()
+		#self.main_window.darea.queue_draw()
 
 	def on_main_window_destroy(self, object, data=None):
 		print("quit")
 		Gtk.main_quit()
+
+# teste, alterar para desenhar objetos
+	def on_viewport_draw(self, widget, cr):
+		x_max = self.main_window.darea.get_allocation().width
+		y_max = self.main_window.darea.get_allocation().height
+
+
+		cr.save()
+		cr.set_source_rgb(0, 0, 0)
+		cr.move_to(0, 0)
+		cr.line_to(0, y_max)
+		cr.line_to(x_max, y_max)
+		cr.line_to(x_max, 0)
+		cr.line_to(0, 0)
+		cr.fill()
+		cr.restore()
+
+		for obj in self.main_window.saved_objects:
+			if obj.object_type == "Point":
+				obj.draw_point(cr)
+
+			elif obj.object_type == "Line":
+				obj.draw_line(cr)
+
+
+
 
 
 class NewObjectHandler:
@@ -54,8 +86,10 @@ class NewObjectHandler:
 		self.main_window = main_window
 		self.builder = main_window.builder
 		self.object_window = object_window
+		self.darea = main_window.darea
 
-	def on_window_new_object_destroy(self, object, data=None):
+
+	def on_window_new_object_destroy(self, object, data=None):				
 		print("new object window closed")
 		self.object_window.destroy()
 
@@ -79,11 +113,23 @@ class NewObjectHandler:
 			x = float(self.builder.get_object("entry_x_point").get_text())
 			y = float(self.builder.get_object("entry_y_point").get_text())
 
-			print(x, y)
 			new_point = objects.Point(x, y, object_id, object_name, "Point")
-			test_point = objects.LinePoint(x, y)
-			print(test_point.x, test_point.y)
-
 			self.main_window.object_list.append([new_point.object_id, new_point.object_name, new_point.object_type])
+			self.main_window.saved_objects.append(new_point)
+
+
+		if current_page == 1:
+			x1 = float(self.builder.get_object("entry_x1_line").get_text())
+			x2 = float(self.builder.get_object("entry_x2_line").get_text())
+			y1 = float(self.builder.get_object("entry_y1_line").get_text())
+			y2 = float(self.builder.get_object("entry_y2_line").get_text())
+
+			new_line = objects.Line(objects.LinePoint(x1, y1), objects.LinePoint(x2, y2), object_id, object_name, "Line")
+			self.main_window.object_list.append([new_line.object_id, new_line.object_name, new_line.object_type])
+			self.main_window.saved_objects.append(new_line)
+
+
+		cr = self.darea.get_window().cairo_create()
+		self.darea.draw(cr)
 
 		self.object_window.destroy()
--- src/objects.py
+++ src/objects.py
@@ -18,12 +18,30 @@ class Point(Object):
 		self.x = x
 		self.y = y
 
+	def draw_point(self, cr):
+		cr.save()
+		cr.set_source_rgb(1, 1, 1)
+		cr.set_line_width(1)
+		cr.move_to(self.x, self.y)
+		cr.line_to(self.x+1, self.y)
+		cr.stroke()
+		cr.restore()
+
 class Line(Object):
 
 	def __init__(self, start_point: LinePoint, end_point: LinePoint, object_id, object_name, object_type):
 		super().__init__(object_id, object_name, object_type)
 		self.start_point = start_point
 		self.end_point = end_point
+
+	def draw_line(self, cr):
+		cr.save()
+		cr.set_source_rgb(1, 1, 1)
+		cr.set_line_width(1)
+		cr.move_to(self.start_point.x, self.start_point.y)
+		cr.line_to(self.end_point.x, self.end_point.y)
+		cr.stroke()
+		cr.restore()
 
 class Polygon(Object):
 
