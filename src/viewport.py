f7a4695ae98345fb8f1bcf7717fad074b0698494
gtarciso <gustavotarciso@hotmail.com>
2019-08-27 11:50:10 -0300

    bug na janela de criação de objetos resolvido

--- src/viewport.py
+++ src/viewport.py
@@ -16,14 +16,21 @@ class Viewport:
 		xw_min = self.window.x_min
 		xw_max = self.window.x_max
 
-		x_vp = ((x - xw_min) / (xw_max - xw_min)) * (self.x_max - self.x_min)
-		return x_vp
+		try:
+			x_vp = ((x - xw_min) / (xw_max - xw_min)) * (self.x_max - self.x_min)
+			return x_vp
 
+		except ZeroDivisionError:
+			pass
 
 
 	def transformY(self, y):
 		yw_min = self.window.y_min
 		yw_max = self.window.y_max
 
-		y_vp = (1 - (y - yw_min) / (yw_max - yw_min)) * (self.y_max - self.y_min) 
-		return y_vp
+		try:
+			y_vp = (1 - (y - yw_min) / (yw_max - yw_min)) * (self.y_max - self.y_min) 
+			return y_vp
+
+		except ZeroDivisionError:
+			pass
